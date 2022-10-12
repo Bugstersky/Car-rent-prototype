4aa3b28bf663cec79d2313edfdabf9ad1826544b
Bugstersky <panherbercik@gmail.com>
2022-10-12 17:11:36 +0200

    image slider repaired, contact.html kicked

--- zasoby/js/app.js
+++ zasoby/js/app.js
@@ -1,50 +1,19 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+const swiper = new Swiper(".swiper", {
+  loop: true,
 
-//Buttons
- const prevBtn = document.querySelector('#prevBtn');
- const nextBtn = document.querySelector('#nextBtn');
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true
+  },
 
-//Counter
-let counter = 1 ;
-const size = carouselImages[1].clientWidth;
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
-carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px';
-
-//Button Listeners
-
-nextBtn.addEventListener('click',() => {
-  console.log('great');
-  if(counter >= carouselImages.length -1) return; 
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px';
-
-})
-
-
-prevBtn.addEventListener('click',() => {
-  if(counter <= 0) return; 
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px';
-
-})
-
-carouselSlide.addEventListener('transitionend ', ()=>{  
-  if(carouselImages[counter].id === 'lastClone'){
-      carouselSlide.style.transition = "none";
-      counter = carouselImages.length-2;
-      carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px';
-
-    }
-
-    if(carouselImages[counter].id === 'firstClone'){
-      carouselSlide.style.transition = "none";
-      counter = carouselImages.length - counter;
-      carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px';
-
-    }
-})
 
 
